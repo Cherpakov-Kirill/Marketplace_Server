d373a81895b0a5533996279c319b19358bded070
Kirill <cherpakov-kirill-42@mail.ru>
2021-11-26 23:30:23 +0700

    First Marketplace Inet implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,4 @@
-import nsu.oop.marketplace.inet.Inet;
-
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Type any number:");
-        Scanner in = new Scanner(System.in);
-        Inet object = new Inet(in.nextInt());
-        System.out.println("Your number is " + object.getA());
     }
 }
