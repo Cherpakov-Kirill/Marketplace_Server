604a66d1a9a510a7ff8ba81a9274f59e9f08f830
Dementor0383 <dmitrykogalenok@yandex.ru>
2021-12-08 23:33:26 +0700

    Add some methods for get tables to client_v1!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,9 @@
 import nsu.oop.marketplace.server.ServerCore;
-import nsu.oop.marketplace.server.database.simpleoperation.TasksOp;
 
 public class Main {
     public static void main(String[] args) {
 
         ServerCore serverCore = new ServerCore(1025, 1000, 2000);
-        TasksOp.getTaskById(3);
+
     }
 }
